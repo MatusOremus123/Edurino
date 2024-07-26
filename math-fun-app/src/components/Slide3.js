7427b9392858505e79afa1c7f61a09c194f13b2e
MatusOremus <matus.oremus@gmail.com>
2024-07-26 15:07:52 +0200

    button fix 2

--- math-fun-app/src/components/Slide3.js
+++ math-fun-app/src/components/Slide3.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const Slide3 = () => {
+const Slide3 = ({ navigate }) => {
   console.log('Rendering Slide3');
   return (
     <div style={styles.container}>
       <div style={styles.imageContainer}>
         <img src="/images/File3.svg" alt="Slide 3" style={styles.image} />
+        <img 
+          src="/images/button.svg" 
+          alt="Next Button" 
+          style={styles.buttonImage} 
+          onClick={() => navigate(4)} 
+        />
       </div>
     </div>
   );
@@ -14,23 +20,30 @@ const Slide3 = () => {
 const styles = {
   container: {
     textAlign: 'center',
-    padding: '20px',
     height: '100vh',
     backgroundColor: '#9fd3c7',
     display: 'flex',
-    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     position: 'relative'
   },
   imageContainer: {
     position: 'relative',
-    width: '80%', 
+    width: '100%', 
+    maxWidth: '400px', 
     height: 'auto'
   },
   image: { 
-    width: '30%', 
+    width: '100%', 
     height: 'auto' 
+  },
+  buttonImage: { 
+    position: 'absolute',
+    bottom: '2%', 
+    right: '3%',  
+    width: '105px',  
+    height: '105px',
+    cursor: 'pointer'
   }
 };
 
